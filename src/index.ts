7ee2ee65ebd9cbb540caa647be23e2e84550cead
Bruno Costa <obrnocsta@gmail.com>
2026-03-31 17:40:13 -0300

    fix: corrige módulos que usam 'vanilla javascript' adicionando tipos TypeScript a eles

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import express from "express";
 import type { Express, Request, Response } from "express";
-
+import cors from "cors";
 import { pets } from "./data/pets";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 const app: Express = express();
+
+app.use(cors());
 
 app.get("/", (req: Request, res: Response): void => {
   res.json(pets);
