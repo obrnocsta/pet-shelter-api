c548dce642f1f96f04b33b16a3656ab57ecad958
Bruno Costa <obrnocsta@gmail.com>
2026-04-01 17:48:42 -0300

    feat: adiciona middleware que simula uma autorização em 'pets.middleware.ts' com a função 'pleaseAuth'

--- src/middlewares/pets.middleware.ts
+++ src/middlewares/pets.middleware.ts
@@ -14,3 +14,18 @@ export const validateNumericId = (
     next();
   }
 };
+
+export const pleaseAuth = (
+  req: Request<{}, unknown, {}, { password?: string }>,
+  res: Response<{ success: boolean; message: string }>,
+  next: NextFunction,
+) => {
+  const { password } = req.query;
+  if (password === "please") {
+    next();
+  } else {
+    res
+      .status(401)
+      .json({ success: false, message: "Unauthorised. You must say 'please'" });
+  }
+};
--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -2,9 +2,9 @@ import express from "express";
 import type { Router } from "express";
 
 import { getPets, getPetById } from "../controllers/pets.controllers";
-import { validateNumericId } from "../middleware/pets.middleware";
+import { validateNumericId, pleaseAuth } from "../middlewares/pets.middleware";
 
 export const petRouter: Router = express.Router();
 
 petRouter.get("/", getPets);
-petRouter.get("/:id", validateNumericId, getPetById);
+petRouter.get("/:id", pleaseAuth, validateNumericId, getPetById);
